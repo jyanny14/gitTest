780cfdc38c8380de00fd7368d4563d944c4bf5b3
차은상 <jyanny14@gmail.com>
2020-08-10 01:29:26 +0900

    병합 테스트 04

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -9,8 +9,8 @@ int main() {
 	int test3 = 0;
 	int test4 = 0;
 
-	cout << "test1 commit1 01";
-	cout << "test1 commit1 02";
+	cout << "test1 commit1 01 merge test 04";
+	cout << "test1 commit1 02 merge test 05";
 	cout << "test1 commit1 03";
 
 	couut << "cherry pick 01";
